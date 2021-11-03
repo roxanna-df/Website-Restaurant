696252ab76630178217b060812c0e7b03952159c
Roxanna De Freitas <68925112+roxanna-df@users.noreply.github.com>
2021-11-03 18:31:10 +0000

    Add files via upload

---
+++ js/bgParallax.js
@@ -0,0 +1,34 @@
+(function(){
+    //Propiedades
+var propParallax = {
+    seccion : document.querySelector('.parallax'),
+    recorrido : null,
+    limite : null
+}
+
+
+//Métodos
+var metParallax = {
+
+    inicio : function(){
+        window.addEventListener('scroll',metParallax.scrollParallax);
+    },
+
+    scrollParallax : function(){
+
+        propParallax.recorrido = window.pageYOffset;
+        propParallax.limite = propParallax.seccion.offsetTop + propParallax.seccion.offsetHeight;
+
+        if(propParallax.recorrido > propParallax.seccion.offsetTop - window.outerHeight && propParallax.recorrido <= propParallax.limite){
+            
+            propParallax.seccion.style.backgroundPositionY = (propParallax.recorrido - propParallax.seccion.offsetTop) / 1.5 + 'px';
+        }else{
+            propParallax.seccion.style.backgroundPositionY = 0;
+        }
+
+    }
+
+}
+
+metParallax.inicio();
+}())
---
+++ js/slider.js
@@ -0,0 +1,30 @@
+(function(){
+    //Propiedades Slider
+var propSlider = {
+    slider : document.getElementById('slider'),
+    primerSlide : null
+}
+
+//Métodos Slider
+var metSlider = {
+
+    inicio : function(){
+        setInterval(metSlider.moverSlide, 3000);
+    },
+
+    moverSlide : function(){
+        propSlider.slider.style.transition = 'all 1s ease';
+        propSlider.slider.style.marginLeft = '-100%';
+
+        setTimeout(function(){
+            propSlider.primerSlide = propSlider.slider.firstElementChild;
+            propSlider.slider.appendChild(propSlider.primerSlide);
+
+            propSlider.primerSlide.style.transition = 'unset';
+            propSlider.primerSlide.style.marginLeft = 0;
+        }, 1000);
+    }
+}
+
+metSlider.inicio();
+}())
